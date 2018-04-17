5e1826812f30d1baf4db12bc7bba28ac923e1d7f
NotHunter101 <35887932+NotHunter101@users.noreply.github.com>
2018-04-17 16:08:03 -0400

    Initial commit

---
+++ Plugin/Plugin.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using Terraria;
+using TerrariaApi.Server;
+using TShockAPI;
+
+namespace PopupChat
+{
+    [ApiVersion(2, 1)]
+    public class Plugin : TerrariaPlugin
+    {
+        public override string Author
+        {
+            get
+            {
+                return "Hunter101";
+            }
+        }
+
+        public override string Description
+        {
+            get
+            {
+                return "A plugin that creates chat above your head (in your role color) when you talk.";
+            }
+        }
+
+        public override string Name
+        {
+            get
+            {
+                return "Pop-up Chat";
+            }
+        }
+
+        public override Version Version
+        {
+            get
+            {
+                return new Version(1, 1);
+            }
+        }
+
+        public Plugin(Main game) : base(game)
+        {
+            Order = 0;
+        }
+
+        public override void Initialize()
+        {
+            ServerApi.Hooks.ServerChat.Register(this, OnChat);
+            Commands.ChatCommands.Add(new Command("popupchat.quiet", Quiet, "quiet"));
+        }
+
+        void Quiet(CommandArgs args)
+        {
+            NetMessage.SendData(119, -1, -1, Terraria.Localization.NetworkText.FromLiteral(String.Join(" ", args.Parameters)), (int)new Color(args.Player.Group.R, args.Player.Group.G, args.Player.Group.B).PackedValue, args.Player.X + 8, args.Player.Y + 32);
+        }
+
+        void OnChat(ServerChatEventArgs args)
+        {
+            if (args.Text.StartsWith(Commands.Specifier) || args.Text.StartsWith(Commands.SilentSpecifier))
+                return;
+            NetMessage.SendData(119, -1, -1, Terraria.Localization.NetworkText.FromLiteral(args.Text), (int)new Color(TShock.Players[args.Who].Group.R, TShock.Players[args.Who].Group.G, TShock.Players[args.Who].Group.B).PackedValue, TShock.Players[args.Who].X + 8, TShock.Players[args.Who].Y + 32);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                ServerApi.Hooks.ServerChat.Deregister(this, OnChat);
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ Plugin/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("PopupChat")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("PopupChat")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("4087f79f-dd2f-46a4-8504-024a194f594b")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
